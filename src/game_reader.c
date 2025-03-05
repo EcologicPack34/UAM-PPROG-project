dc36b27fc9a39b4ca6951972f84761c227c8c780
killuaEG832 <maksimiliano.pok178@gmail.com>
2025-03-05 20:43:52 +0100

    FIX to game_reader

--- src/game_reader.c
+++ src/game_reader.c
@@ -116,6 +116,10 @@ Status game_reader_create_from_file(Game **game, char *filename){
     debug_log(LOG_ERROR, "Error loading events at: game_reader_create_from_file(Game*, char*) in game_reader.c");
     return ERROR;
   }
+  if (game_reader_load_npcs(*game, filename) == ERROR){
+    debug_log(LOG_ERROR, "Error loading npcs at: game_reader_create_from_file(Game*, char*) in game_reader.c");
+    return ERROR;
+  }
 
   return OK;
 }
