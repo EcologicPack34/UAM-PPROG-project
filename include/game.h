622c656cf3a7c52f222d5dd5989b3589076f6ef8
mandrilus <aaroncete8@hotmail.com>
2025-05-03 20:12:01 +0200

    daw

--- include/game.h
+++ include/game.h
@@ -239,16 +239,6 @@ EventManager *game_get_event_manager(Game *game);
 Collection *game_get_npcs(Game *game);
 
 /**
- * @brief it gets an NPC from it's id in a Game
- * @author Aaron Charameli Mair
- * 
- * @param game game struct
- * @param id the NPC's Id
- * @return NPC* or NULL if error 
- */
-NPC *game_get_npc_by_id(Game *game, Id id);
-
-/**
  * @brief Gets player by id
  * @author Aaron Charameli Mair
  * 
